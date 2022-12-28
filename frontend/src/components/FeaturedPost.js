7b8ebdc2c2059e18de5e40c49624b81c73f067ca
EricTsengTy <eric.tsengty@gmail.com>
2022-12-28 17:55:12 +0800

    [Frontend] Add posts page

---
+++ frontend/src/components/FeaturedPost.js
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+
+
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+
+const trim = (content, limit) => {
+    const length = content.length;
+    if (content.indexOf("\n") !== -1)
+        content = content.substring(0, content.indexOf('\n'));
+
+    if (length <= limit)
+        return content;
+
+    content = content.substring(0, limit);
+    return content + " ...";
+}
+
+/* Ref. https://github.com/mui/material-ui/blob/v5.11.2/docs/data/material/getting-started/templates/blog/FeaturedPost.js */
+const FeaturedPost = ({ post, onClick}) => {
+	let { title, username, content } = post
+
+	content = trim(content, 40);
+
+	return (
+		<Grid item xs={7}>
+			<CardActionArea onClick={onClick}>
+				<Card sx={{ display: 'flex' }}>
+					<CardContent sx={{ flex: 1 }}>
+						<Typography component="h2" variant="h5">
+							{title ?? "No title"}
+						</Typography>
+						<Typography variant="subtitle1" color="text.secondary">
+							{username ?? "Anonymous"}
+						</Typography>
+						<Typography variant="subtitle1" color="text.secondary">
+							{content}
+						</Typography>
+					</CardContent>
+				</Card>
+			</CardActionArea>
+		</Grid>
+	);
+}
+
+export default FeaturedPost;
--- frontend/src/containers/Posts.js
+++ frontend/src/containers/Posts.js
@@ -1,8 +1,33 @@
+import { useNavigate, useLocation } from 'react-router-dom'
+import { useQuery } from '@apollo/client';
+import Grid from '@mui/material/Grid';
+
+import { POSTS_QUERY } from '../graphql';
+import FeaturedPost from '../components/FeaturedPost';
+
 const Posts = () => {
+    const navigate = useNavigate();
+
+    const {
+        loading, error, data: postsData
+    } = useQuery(POSTS_QUERY)
+    const { posts } = loading ? { posts: [] } : postsData;
+
     return (
-        <div>
-            <h2>Posts Page</h2>
-        </div>
+        <>
+            <Grid container spacing={1} 
+                justifyContent="center"
+                alignItems="center"
+            >
+                {posts.map((post, index) =>
+                    <FeaturedPost 
+                        key={post.PID} 
+                        post={post} 
+                        onClick={() => navigate(`/post/${post.PID}`)} 
+                    />
+                )}
+            </Grid>
+        </>
     )
 }
 
