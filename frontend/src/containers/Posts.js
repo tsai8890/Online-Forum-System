0bc4727c8e9aec7ddda7a7299d6b0d6c2770eeaf
EricTsengTy <eric.tsengty@gmail.com>
2022-12-28 22:50:57 +0800

    [Frontend] Add profile page

--- frontend/src/containers/Posts.js
+++ frontend/src/containers/Posts.js
@@ -1,4 +1,4 @@
-import { useNavigate, useLocation } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { useQuery } from '@apollo/client';
 import Grid from '@mui/material/Grid';
 
--- frontend/src/containers/Profile.js
+++ frontend/src/containers/Profile.js
@@ -1,8 +1,53 @@
+import { useNavigate } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+import { useQuery } from '@apollo/client';
+import Grid from '@mui/material/Grid';
+
+import { POSTS_BY_UID_QUERY, USER_BY_UID_QUERY } from '../graphql';
+import FeaturedPost from '../components/FeaturedPost';
+import UserInfo from '../components/MainFeaturedPost';
+
 const Profile = () => {
+    const { id: UID } = useParams();
+    const navigate = useNavigate();
+    
+    const {
+        loading: postsLoading, data: postsData
+    } = useQuery(POSTS_BY_UID_QUERY, {
+        variables: {UID},
+    });
+
+    const {
+        loading: userLoading, data: userData
+    } = useQuery(USER_BY_UID_QUERY, {
+        variables: {UID},
+    });
+
+
+    const { postsByUID: posts } = postsLoading || !postsData 
+        ? { postsByUID: [] } 
+        : postsData;
+
+    const { userByUID: user } = userLoading || !userData
+        ? { userByUID: [] }
+        : userData;
+
     return (
-        <div>
-            <h2>Profile Page</h2>
-        </div>
+        <>  
+            <UserInfo user={user} />
+            <Grid container spacing={1} 
+                justifyContent="center"
+                alignItems="center"
+            >
+                {posts.map((post, index) =>
+                    <FeaturedPost 
+                        key={post.PID} 
+                        post={post} 
+                        onClick={() => navigate(`/post/${post.PID}`)} 
+                    />
+                )}
+            </Grid>
+        </>
     )
 }
 
