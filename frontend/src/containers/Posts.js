f684284dd38393109ed807fe9b5561ff65c437be
tsai8890 <asdpeter6520@gmail.com>
2022-12-30 02:20:04 +0800

    [CreatePost]
    - CreatePost.js
    	- a page to create posts
    - Posts.js
    	- a write button linking to createPost.js

--- frontend/src/containers/Posts.js
+++ frontend/src/containers/Posts.js
@@ -4,10 +4,12 @@ import Grid from '@mui/material/Grid';
 
 import { POSTS_QUERY } from '../graphql';
 import PostItem from '../components/PostItem';
+import { Button, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 
 const Posts = () => {
     const navigate = useNavigate();
-
+    
     const {
         loading, error, data: postsData
     } = useQuery(POSTS_QUERY);
@@ -16,6 +18,27 @@ const Posts = () => {
 
     return (
         <>
+            <Grid container justifyContent="center" wrap="nowrap">
+                <Grid container item justifyContent="flex-start"
+                    sx={{
+                        width: "730px", 
+                        maxWidth: "730px", 
+                        margin: "20px", 
+                    }}
+                >
+                    <Grid item>
+                        <Typography variant="h4" >
+                            All Posts
+                        </Typography>
+                    </Grid>
+
+                    <Grid conatiner item sx={{marginLeft: "70%"}}>
+                        <Button variant="outlined" onClick={()=>navigate("/createPost")}>
+                            Write 
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Grid>
             <Grid container spacing={1} 
                 justifyContent="center"
                 alignItems="center"
