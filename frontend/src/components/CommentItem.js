7b55f856397598087d473539f3ed1f1b077b6332
Hsu-Huai, Tsai <56215518+tsai8890@users.noreply.github.com>
2022-12-31 15:46:43 +0800

    [Fix] fix the positioning of the comment (#24)

--- frontend/src/components/CommentItem.js
+++ frontend/src/components/CommentItem.js
@@ -6,6 +6,7 @@ import Grid from '@mui/material/Grid';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
 import FaceIcon from '@mui/icons-material/Face';
+import { Divider } from '@mui/material';
 
 /* Ref. https://github.com/mui/material-ui/blob/v5.11.2/docs/data/material/getting-started/templates/blog/FeaturedPost.js */
 const CommentItem = ({ comment }) => {
@@ -14,15 +15,19 @@ const CommentItem = ({ comment }) => {
 
 	return (
 		<Grid item xs={12}>
-            <Stack direction="row" spacing={1}>
-                <Chip icon={<FaceIcon />} label={nickname} variant="outlined" />
+            <Stack direction="row" spacing={1} sx={{ marginBottom: "10px"}}>
+                <Grid sx={{ width: "120px" }}>
+                    <Chip icon={<FaceIcon />} label={nickname} variant="outlined" />
+                </Grid>
                 <Typography 
                     variant="p" 
                     color="text.secondary" 
-                    paragraph 
-                    style={{ wordWrap: "break-word" }} 
                     alignSelf="center"
-                    sx={{whiteSpace: "pre"}}
+                    sx={{
+                        whiteSpace: "pre-wrap", 
+                        wordWrap: "break-word", 
+                        width: "600px", 
+                    }}
                 >
                     {message}
                 </Typography>
