8e122607520bb50d0ac3fc93a9612bb440467014
Tzu-Yen, Tseng <eric.tsengty@gmail.com>
2023-01-02 18:13:26 +0800

    [Feature] Add requirements for deployment (#30)

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
 import mongo from './mongo';
 import server from './server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import express from 'express'
+import { expressMiddleware } from '@apollo/server/express4';
+import { json } from 'body-parser';
+import path from 'path'
+import cors from 'cors';
 
 import PostModel from './model/Post';
 import UserModel from './model/User';
@@ -9,14 +14,29 @@ mongo.connect();
 
 const PORT = process.env.PORT || 4000;
 // const HOST = 'localhost'
+const app = express();
 
-startStandaloneServer(server, {
-    context: async ({ req }) => ({ 
-        token: req.headers.token,
-        PostModel: PostModel,
-        UserModel: UserModel,
-    }),
-    listen: { port: PORT },
-}).then(({url}) => {
-    console.log(`Apollo server is up on port ${PORT}`);
+if (process.env.NODE_ENV === "production") {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, "../frontend", "build")));
+    app.get("/*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+    });
+}
+
+const startServer = async () => {
+    await server.start();
+    app.use('/graphql', cors(), json(), expressMiddleware(server, {
+        context: async ({ req }) => ({ 
+            token: req.headers.token,
+            PostModel: PostModel,
+            UserModel: UserModel,
+        }),
+    }));
+}
+
+startServer();
+
+app.listen({port: PORT}, () => {
+    console.log(`Apollo server is up on port ${PORT}`)
 });
